ca8adfbcd83a67229a952d58dd443a5d844ec154
felipejoq <felipejofreq@gmail.com>
2024-10-15 12:03:09 -0300

    config cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare(),
   devToolbar: {
     enabled: false
