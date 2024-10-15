3215b4c3aece3311fb706cd6e5402cf13a66c4cf
felipejoq <felipejofreq@gmail.com>
2024-10-15 12:11:01 -0300

    cambios de mensajes informativos para el usuario en el formulario de la imagen

--- src/components/uploads/UploadImage.tsx
+++ src/components/uploads/UploadImage.tsx
@@ -60,14 +60,22 @@ export const UploadImage: React.FC = () => {
                 loading ? (<Loader/>) : (
                     <form onSubmit={handleSubmit}
                           className="flex flex-col gap-5 items-center justify-center content-center w-full">
+
                         <input
+                            id='file'
                             type="file"
                             name="file"
                             onChange={handleFile}
                             className='text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-pink-50 file:text-pink-700 hover:file:bg-pink-100'
                         />
+                        <label htmlFor='file'>
+                            Solo archivos .jgp, jpeg y png de máximo 2MB.
+                        </label>
                         <label htmlFor='change_bg'
                                className='text-sm text-slate-500 flex flex-col sm:flex-row items-center justify-center gap-2'>
+                            <div className='flex gap-5 text-orange-500 font-semibold text-2xl'>
+                                Marca para agregar un fondo "Terrorífico"</div>
+                                <span className='hidden sm:block text-[30px]'>😏👉</span>
                             <div className='flex justify-center'>
                                 <input
                                     checked={changeBg}
@@ -76,10 +84,8 @@ export const UploadImage: React.FC = () => {
                                     name='change_bg'
                                     id='change_bg'
                                     className='mr-2 h-5 w-5 text-orange-500 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 focus:ring-2'
-                                /> <span className='block sm:hidden text-[30px]'>👈</span>
+                                /> <span className='block sm:hidden text-[30px]'>👈😏</span>
                             </div>
-                            <span className='flex gap-5 text-orange-500 font-semibold text-2xl'> <span
-                                className='hidden sm:block'>👈</span> Marca para agregar un fondo "Terrorífico" 😏</span>
                         </label>
 
                         <button
