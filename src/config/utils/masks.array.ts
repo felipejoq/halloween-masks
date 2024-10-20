1ad82a4a26a017593eb7f8e9e0286875bcf912ad
felipejoq <felipejofreq@gmail.com>
2024-10-20 15:18:15 -0300

    optimización de imagenes

--- src/config/utils/masks.array.ts
+++ src/config/utils/masks.array.ts
@@ -4,9 +4,9 @@ export interface Mask {
 }
 
 export const masks: Mask[] = [
-    {public_id: 'masks:mask_1', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1728763807/masks/mask_1.png"},
+    {public_id: 'masks:mask_1', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1729448116/masks/mask_1.webp"},
     {public_id: 'masks:mask_2', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1728698903/masks/mask_2.webp"},
-    {public_id: 'masks:mask_3', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1728698904/masks/mask_3.png"},
+    {public_id: 'masks:mask_3', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1729447957/masks/mask_3.webp"},
     {public_id: 'masks:mask_4', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1729092637/masks/mask_4.webp"},
-    {public_id: 'masks:mask_5', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1728698907/masks/mask_5.webp"},
+    {public_id: 'masks:mask_5', src: "https://res.cloudinary.com/dzwgql0ie/image/upload/v1729447861/masks/mask_5.webp"},
 ];
